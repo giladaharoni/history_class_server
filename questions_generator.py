5a81465b83851960547863d15022ba83020086d0
giladaharoni <63557212+giladaharoni@users.noreply.github.com>
2024-03-26 12:49:23 +0200

    Update questions_generator.py

--- questions_generator.py
+++ questions_generator.py
@@ -5,7 +5,7 @@ def which_one_participant_in_the_event(lesson_id, cursor):
     ## select relevant event, pick one as random
     query = f"""
         SELECT ID, Title
-        FROM mydb.historical_event as a, mydb.lesson as b, mydb.lesson_country as c, mydb.event_figures as d
+        FROM db02.historical_event as a, db02.lesson as b, db02.lesson_country as c, db02.event_figures as d
         WHERE b.idlesson = {lesson_id}  and c.idlesson = {lesson_id} and a.year < b.end_time and a.year > b.start_time and 
         a.country = c.idcountry and d.idevent=a.ID
         ORDER BY RAND () LIMIT 1"""
@@ -14,7 +14,7 @@ def which_one_participant_in_the_event(lesson_id, cursor):
     ### pick someone who participate in it
     query = f"""
     SELECT Name
-    FROM mydb.event_figures as a, mydb.figure as b
+    FROM db02.event_figures as a, db02.figure as b
     WHERE a.idevent = {event[0][0]} and a.idfigure = b.ID
     ORDER BY RAND () LIMIT 1
     """
@@ -23,19 +23,19 @@ def which_one_participant_in_the_event(lesson_id, cursor):
     ### pick three who doesn't but from the same countries and years
     query = f"""
     SELECT Name
-    FROM mydb.figure as a,mydb.lesson as b, mydb.lesson_country as c
-    WHERE a.ID not in (SELECT idfigure FROM mydb.event_figures WHERE idevent = {event[0][0]}) and b.idlesson = 
+    FROM db02.figure as a,db02.lesson as b, db02.lesson_country as c
+    WHERE a.ID not in (SELECT idfigure FROM db02.event_figures WHERE idevent = {event[0][0]}) and b.idlesson = 
     c.idlesson and b.idlesson = {lesson_id} and (((a.year_birth <= b.start_time and a.year_death >= b.start_time ) or (a.year_death <= b.end_time and a.year_birth >= b.start_time) or (a.year_birth >= b.start_time and a.year_birth <=b.end_time))
      and (a.birth_country = c.idcountry or a.death_country = c.idcountry))
      ORDER BY RAND () LIMIT 3
     """
     cursor.execute(query)
     event_title = event[0][1]
-    question = f"Who take part in {event_title}?"
+    question = f"Who took part in {event_title}?"
     wrong_answers = [i[0] for i in cursor.fetchall()]
-    answers = wrong_answers+figure
+    answers = wrong_answers+list(figure[0])
     random.shuffle(answers)
-    right_answer = figure[0]
+    right_answer = figure[0][0]
     return {"question": question, "options": answers, "right_answer": right_answer}
 
 
@@ -43,7 +43,7 @@ def which_occor_in_the_year(lesson_id, cursor):
     ## select relevant event, pick one as random
     query = f"""
             SELECT ID, Title, year
-            FROM mydb.historical_event as a, mydb.lesson as b, mydb.lesson_country as c, mydb.event_figures as d
+            FROM db02.historical_event as a, db02.lesson as b, db02.lesson_country as c, db02.event_figures as d
             WHERE b.idlesson = {lesson_id}  and c.idlesson = {lesson_id} and a.year < b.end_time and a.year > b.start_time and 
             a.country = c.idcountry and d.idevent=a.ID
             ORDER BY RAND () LIMIT 1"""
@@ -52,7 +52,7 @@ def which_occor_in_the_year(lesson_id, cursor):
     ### pick three other events from the same time and country of the lesson
     query = f"""
         SELECT Title
-        FROM mydb.historical_event as a,mydb.lesson as b, mydb.lesson_country as c
+        FROM db02.historical_event as a,db02.lesson as b, db02.lesson_country as c
         WHERE a.ID != {event[0][0]} and b.idlesson = 
         c.idlesson and b.idlesson = {lesson_id} and a.Year between b.start_time and b.end_time
          ORDER BY RAND () LIMIT 3
@@ -60,16 +60,16 @@ def which_occor_in_the_year(lesson_id, cursor):
     cursor.execute(query)
     event_year = event[0][2]
     question = f"which event happend in {event_year}?"
-    answers = cursor.fetchall() + [event[0][1]]
+    answers = [i[0] for i in cursor.fetchall()] + [event[0][1]]
     random.shuffle(answers)
-    right_answer = event
+    right_answer = event[0][1]
     return {"question": question, "options": answers, "right_answer": right_answer}
 
 def who_died_in(lesson_id, cursor):
     ## select a random figure
     query = f"""
     SELECT Name, year_death
-    FROM mydb.figure as a,mydb.lesson as b, mydb.lesson_country as c
+    FROM db02.figure as a,db02.lesson as b, db02.lesson_country as c
     WHERE b.idlesson = 
     c.idlesson and b.idlesson = {lesson_id} and (((a.year_birth <= b.start_time and a.year_death >= b.start_time ) or (a.year_death <= b.end_time and a.year_birth >= b.start_time) or (a.year_birth >= b.start_time and a.year_birth <=b.end_time))
      and (a.birth_country = c.idcountry or a.death_country = c.idcountry)) and a.year_death is not null
@@ -84,25 +84,25 @@ def who_died_in(lesson_id, cursor):
     ### pick three who doesn't but from the same countries and years
     query = f"""
 SELECT name
-        FROM mydb.figure as a,mydb.lesson as b, mydb.lesson_country as c
-        WHERE a.ID not in (SELECT idfigure FROM mydb.event_figures WHERE year_death = {figure_death}) and b.idlesson = 
+        FROM db02.figure as a,db02.lesson as b, db02.lesson_country as c
+        WHERE a.ID not in (SELECT idfigure FROM db02.event_figures WHERE year_death = {figure_death}) and b.idlesson = 
         c.idlesson and b.idlesson = {lesson_id} and (((a.year_birth <= b.start_time and a.year_death >= b.start_time ) or (a.year_death <= b.end_time and a.year_birth >= b.start_time) or (a.year_birth >= b.start_time and a.year_birth <=b.end_time))
          and (a.birth_country = c.idcountry or a.death_country = c.idcountry))
          ORDER BY RAND () LIMIT 3
         """
     cursor.execute(query)
     question = f"Who died in {figure_death}?"
-    answers = cursor.fetchall() + [figure_name]
+    answers = [ans[0] for ans in cursor.fetchall()] + [figure_name]
     random.shuffle(answers)
     right_answer = [figure_name]
-    return {"question": question, "options": answers, "right_answer": right_answer}
+    return {"question": question, "options": answers, "right_answer": right_answer[0]}
 
 
 def who_born_in(lesson_id, cursor):
     ## select a random figure
     query = f"""
         SELECT Name, year_birth
-        FROM mydb.figure as a,mydb.lesson as b, mydb.lesson_country as c
+        FROM db02.figure as a,db02.lesson as b, db02.lesson_country as c
         WHERE b.idlesson = 
         c.idlesson and b.idlesson = {lesson_id} and (((a.year_birth <= b.start_time and a.year_death >= b.start_time ) or (a.year_death <= b.end_time and a.year_birth >= b.start_time) or (a.year_birth >= b.start_time and a.year_birth <=b.end_time))
          and (a.birth_country = c.idcountry or a.death_country = c.idcountry))
@@ -117,8 +117,8 @@ def who_born_in(lesson_id, cursor):
     ### pick three who doesn't but from the same countries and years
     query = f"""
     SELECT name
-            FROM mydb.figure as a,mydb.lesson as b, mydb.lesson_country as c
-            WHERE a.ID not in (SELECT idfigure FROM mydb.event_figures WHERE year_birth = {figure_birth}) and b.idlesson = 
+            FROM db02.figure as a,db02.lesson as b, db02.lesson_country as c
+            WHERE a.ID not in (SELECT idfigure FROM db02.event_figures WHERE year_birth = {figure_birth}) and b.idlesson = 
             c.idlesson and b.idlesson = {lesson_id} and (((a.year_birth <= b.start_time and a.year_death >= b.start_time ) or (a.year_death <= b.end_time and a.year_birth >= b.start_time) or (a.year_birth >= b.start_time and a.year_birth <=b.end_time))
              and (a.birth_country = c.idcountry or a.death_country = c.idcountry))
              ORDER BY RAND () LIMIT 3
@@ -130,13 +130,13 @@ def who_born_in(lesson_id, cursor):
     random.shuffle(answers)
     right_answer = [figure_name]
 
-    return {"question": question, "options": answers, "right_answer": right_answer}
+    return {"question": question, "options": answers, "right_answer": right_answer[0]}
 
 
 def figure_occupation(lesson_id, cursor):
     query = f"""
     SELECT a.Name, a.ID, e.name
-    FROM mydb.figure as a,mydb.lesson as b, mydb.lesson_country as c, figure_occupation as d, occupation as e
+    FROM db02.figure as a,db02.lesson as b, db02.lesson_country as c, figure_occupation as d, occupation as e
     WHERE b.idlesson = c.idlesson and b.idlesson = {lesson_id} and (((a.year_birth <= b.start_time and a.year_death >= 
     b.start_time ) or (a.year_death <= b.end_time and a.year_birth >= b.start_time) or (a.year_birth >= b.start_time and a.year_birth <=b.end_time))
     and (a.birth_country = c.idcountry or a.death_country = c.idcountry)) and d.idfigure = a.ID and e.idoccupation = d.idoccupation
@@ -164,24 +164,27 @@ def figure_occupation(lesson_id, cursor):
 def country_in_wars(lesson_id, cursor):
     query = f"""SELECT c.idwar, c.idcountry, e.Title, d.country FROM 
     (SELECT idwar, idcountry
-    FROM war_participants as a,(SELECT idcountry FROM mydb.lesson_country WHERE idlesson={lesson_id}) as b 
+    FROM war_participants as a,(SELECT idcountry FROM db02.lesson_country WHERE idlesson={lesson_id}) as b 
     WHERE b.idcountry = a.idparticipantscol) as c, countries as d, historical_period as e
     WHERE c.idcountry = d.ID and e.ID = c.idwar
     order by rand() limit 1"""
     cursor.execute(query)
     war_country_pair = cursor.fetchall()
-    country_id = war_country_pair[0][1]
-    country_name = war_country_pair[0][3]
-    war_name = war_country_pair[0][2]
-    sec_query = f"""
-    SELECT Title
-    FROM historical_period, war_participants
-    WHERE historical_period.ID = war_participants.idwar and war_participants.idwar not in (SELECT idwar FROM 
-    war_participants WHERE idparticipantscol={country_id})
-    order by rand() limit 3"""
-    cursor.execute(sec_query)
-    another_wars = cursor.fetchall()
-    question = "In which wars, did " + country_name + " take part of?"
-    answers = [war[0] for war in another_wars] + [war_name]
-    random.shuffle(answers)
-    return {"question": question, "options": answers, "right_answer": war_name}
+    try:
+        country_id = war_country_pair[0][1]
+        country_name = war_country_pair[0][3]
+        war_name = war_country_pair[0][2]
+        sec_query = f"""
+        SELECT Title
+        FROM historical_period, war_participants
+        WHERE historical_period.ID = war_participants.idwar and war_participants.idwar not in (SELECT idwar FROM 
+        war_participants WHERE idparticipantscol={country_id})
+        order by rand() limit 3"""
+        cursor.execute(sec_query)
+        another_wars = cursor.fetchall()
+        question = "In which wars, did " + country_name + " take part of?"
+        answers = [war[0] for war in another_wars] + [war_name]
+        random.shuffle(answers)
+        return {"question": question, "options": answers, "right_answer": war_name}
+    except IndexError:
+        return {"question": "question", "options": [], "right_answer": "2"}
